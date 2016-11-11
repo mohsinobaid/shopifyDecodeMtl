a772cbc8b72180a9efbeddfdcf7a3090c48835ff
kirinrastogi <rastogikirin@gmail.com>
2016-11-11 17:26:51 -0500

    Add Metafields controller

---
+++ app/controllers/metafields_controller.rb
@@ -0,0 +1,2 @@
+class MetafieldsController < ApplicationController
+end
---
+++ test/controllers/metafields_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MetafieldsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
