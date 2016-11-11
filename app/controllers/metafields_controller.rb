b39feaf0308a5f101bba6ac25de2e201726f1977
kirinrastogi <rastogikirin@gmail.com>
2016-11-11 18:11:27 -0500

    Add route for metafields with GET and /:email

--- app/controllers/metafields_controller.rb
+++ app/controllers/metafields_controller.rb
@@ -1,2 +1,12 @@
 class MetafieldsController < ApplicationController
+    def new
+        @metafield = Metafield.new
+    end
+
+    def show
+        json_response = {
+            user_email: params[:email]
+        }
+        render json: json_response
+    end
 end
