e0475ae5a141c3166aba8588b1d27a65e6ada27c
kirinrastogi <rastogikirin@gmail.com>
2016-11-11 20:45:16 -0500

    Add base for create in metafield controller&router

--- app/controllers/metafields_controller.rb
+++ app/controllers/metafields_controller.rb
@@ -5,7 +5,6 @@ class MetafieldsController < ApplicationController
         # Params:
         # +id+:: id used to identify the user in the metafield
 
-
         begin
             field = ShopifyAPI::Metafield.find(params[:id])
         rescue
@@ -28,5 +27,14 @@ class MetafieldsController < ApplicationController
             metafields: fields
         }
         render json: json_response
+    end
+
+    def create
+        # Receives the metafield, and posts/puts based on if the metafield exists in the store
+        # such that the metafield will be updated and not overwritten if it exists
+        # /metafields/??
+        # Params:
+        # +metafield+:: metafield used to be created or updated
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :metafields, only: [:show, :index]
+  resources :metafields, only: [:show, :index, :create]
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # The priority is based upon order of creation: first created -> highest priority.
