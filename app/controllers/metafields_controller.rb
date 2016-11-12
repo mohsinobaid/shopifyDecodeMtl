c27eabaabaecb909022370b0b9ea1f758fe8134f
kirinrastogi <rastogikirin@gmail.com>
2016-11-11 19:54:36 -0500

    Add GET routing and controllers for show and index

--- app/controllers/metafields_controller.rb
+++ app/controllers/metafields_controller.rb
@@ -1,11 +1,31 @@
 class MetafieldsController < ApplicationController
-    def new
-        @metafield = Metafield.new
+    def show
+        # Responds with found metafield corresponding to id, or an empty string
+        # /metafields/id
+        # Params:
+        # +id+:: id used to identify the user in the metafield
+
+
+        begin
+            field = ShopifyAPI::Metafield.find(params[:id])
+        rescue
+            field = ''
+        end
+        
+        json_response = {
+            metafield: field
+        }
+
+        render json: json_response
     end
 
-    def show
+    def index
+        # Responds with entire list of metafields from the store
+        # /metafields
+
+        fields = ShopifyAPI::Metafield.all
         json_response = {
-            user_email: params[:email]
+            metafields: fields
         }
         render json: json_response
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/metafields/:email' => 'metafields#show'
+  resources :metafields, only: [:show, :index]
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # The priority is based upon order of creation: first created -> highest priority.
